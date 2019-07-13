25d295ee9d5d05e0f4057bd06e77926529e9e8ef
Katarina Ljungdahl <katarina@earthpeople.se>
2019-07-13 17:20:25 +0200

    Let crossword extend component to be able to scroll on mount

--- src/components/crossword.js
+++ src/components/crossword.js
@@ -1,94 +1,121 @@
-import React from "react"
+import React, { Component } from "react"
 import data from "./../data"
 import styles from "./crossword.module.css"
 
-const Crossword = () => {
-  const height = data.length
+class Crossword extends Component {
+  componentDidMount() {
+    if (this._grid && this._cell) {
+      let parent = this._cell
+      let offset = 0
 
-  let left = 0
-  let right = 0
+      while (parent) {
+        offset += parent.offsetLeft
+        parent = parent.parentElement
+      }
 
-  const grid = []
+      this._grid.scrollIntoView({ block: "center" })
 
-  for (let i = 0; i < height; i++) {
-    grid[i] = []
-    const index = data[i].findIndex(item => item.includes("_"))
-    const after = data[i].length - index
-
-    if (left < index) left = index
-    if (right < after) right = after
+      this._grid.scrollTo(offset / 2 - this._cell.offsetWidth, 0)
+    }
   }
 
-  /* 1 Since middle letter needs to be included in total width */
-  const width = left + right
+  render() {
+    const height = data.length
 
-  grid.forEach((row, rowIndex) => {
-    const index = data[rowIndex].findIndex(item => item.includes("_"))
-    const wordLength = data[rowIndex].length
+    let left = 0
+    let right = 0
 
-    let leftStart
-    let diff
+    const grid = []
 
-    for (let i = 0; i < width; i++) {
-      if (i < left - index || i > left - index + wordLength - 1) {
-        row[i] = null
-      } else {
-        /* Store difference between left and first letter's index */
-        if (i - left < 0 && !leftStart) {
-          leftStart = true
-          diff = left - i
-        }
+    for (let i = 0; i < height; i++) {
+      grid[i] = []
+      const index = data[i].findIndex(item => item.includes("_"))
+      const after = data[i].length - index
 
-        if (leftStart) row[i] = [rowIndex, i - left + diff]
-        else row[i] = [rowIndex, i - left]
-      }
+      if (left < index) left = index
+      if (right < after) right = after
     }
-  })
 
-  let labelIndex
-  let currentRow
+    const width = left + right
 
-  return (
-    <div className={styles.grid}>
-      {grid.map((row, rowIndex) => {
-        return (
-          <div className={styles.row}>
-            {row.map((item, itemIndex) => {
-              if (!item) return <div className={styles.placeholder}></div>
+    grid.forEach((row, rowIndex) => {
+      const index = data[rowIndex].findIndex(item => item.includes("_"))
+      const wordLength = data[rowIndex].length
 
-              const value = data[item[0]][item[1]]
-              const vertical = value.includes("_")
+      let leftStart
+      let diff
 
-              if (currentRow !== rowIndex) {
-                labelIndex = itemIndex
-                currentRow = rowIndex
-              }
+      for (let i = 0; i < width; i++) {
+        if (i < left - index || i > left - index + wordLength - 1) {
+          row[i] = null
+        } else {
+          /* Store difference between left and first letter's index */
+          if (i - left < 0 && !leftStart) {
+            leftStart = true
+            diff = left - i
+          }
 
-              return (
-                <div className={styles.inputContainer}>
-                  {labelIndex === itemIndex ? (
-                    <label className={styles.label}>{rowIndex + 1}</label>
-                  ) : null}
-                  <input
-                    id={`item-${item[0]}-${item[1]}`}
-                    className={`${styles.input} ${
-                      vertical ? styles.vertical : null
-                    }`}
-                    type="text"
-                    minLength="1"
-                    required={vertical}
-                    maxLength="1"
-                    autoComplete="off"
-                    pattern="^[aA-öÖ]{1}$"
-                  ></input>
-                </div>
-              )
-            })}
-          </div>
-        )
-      })}
-    </div>
-  )
+          if (leftStart) row[i] = [rowIndex, i - left + diff]
+          else row[i] = [rowIndex, i - left]
+        }
+      }
+    })
+
+    let labelIndex
+    let currentRow
+
+    return (
+      <div className={styles.grid} ref={el => (this._grid = el)}>
+        {grid.map((row, rowIndex) => {
+          return (
+            <div className={styles.row} key={`row-${rowIndex}`}>
+              {row.map((cell, cellIndex) => {
+                if (!cell)
+                  return (
+                    <div
+                      key={`placeholder-${cellIndex}`}
+                      className={styles.placeholder}
+                    ></div>
+                  )
+
+                const value = data[cell[0]][cell[1]]
+                const vertical = value.includes("_")
+
+                if (currentRow !== rowIndex) {
+                  labelIndex = cellIndex
+                  currentRow = rowIndex
+                }
+
+                return (
+                  <div
+                    key={`cell-${cellIndex}`}
+                    className={styles.cell}
+                    ref={el => (!this._cell ? (this._cell = el) : null)}
+                  >
+                    {labelIndex === cellIndex ? (
+                      <label className={styles.label}>{rowIndex + 1}</label>
+                    ) : null}
+                    <input
+                      id={`item-${cell[0]}-${cell[1]}`}
+                      className={`${styles.input} ${
+                        vertical ? styles.vertical : null
+                      }`}
+                      type="text"
+                      minLength="1"
+                      required={vertical}
+                      maxLength="1"
+                      autoComplete="off"
+                      pattern="^[aA-öÖ]{1}$"
+                    ></input>
+                  </div>
+                )
+              })}
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
 }
 
 export default Crossword
