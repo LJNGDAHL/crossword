6c14419d40a2fa4a77a1c09814fe8ae0e140006b
Katarina Ljungdahl <katarina@earthpeople.se>
2019-07-15 11:37:16 +0200

    Temporarily comment out unused code since crossword structure has changed

--- src/components/crossword.js
+++ src/components/crossword.js
@@ -10,161 +10,127 @@ class Crossword extends Component {
       pressed: null,
     }
 
-    this.handleKeyDown = this.handleKeyDown.bind(this)
-    this.handleKeyUp = this.handleKeyUp.bind(this)
+    // this.handleKeyDown = this.handleKeyDown.bind(this)
+    // this.handleKeyUp = this.handleKeyUp.bind(this)
     this.handleChange = this.handleChange.bind(this)
   }
 
-  componentDidMount() {
-    // Center first cell on mount
-    if (this._form && this._cell) {
-      let parent = this._cell
-      let offset = 0
+  // componentDidMount() {
+  //   // Center first cell on mount
+  //   if (this._form && this._cell) {
+  //     let parent = this._cell
+  //     let offset = 0
 
-      while (parent) {
-        offset += parent.offsetLeft
-        parent = parent.parentElement
-      }
+  //     while (parent) {
+  //       offset += parent.offsetLeft
+  //       parent = parent.parentElement
+  //     }
 
-      this._form.scrollTo(offset / 2 - this._cell.offsetWidth, 0)
-    }
-  }
+  //     this._form.scrollTo(offset / 2 - this._cell.offsetWidth, 0)
+  //   }
+  // }
 
   // Check if user has filled in all the required fields
   handleChange() {
     const valid = this._form.checkValidity()
+    console.log(this._form)
     this.props.onChange(valid)
   }
 
-  handleKeyDown(event) {
-    /* Stored pressed key to avoid buggy behaviour
-     * whenever user hits multiple keys at the same time
-     */
-    this.setState({ pressed: event.key })
+  // handleKeyDown(event) {
+  //   /* Stored pressed key to avoid buggy behaviour
+  //    * whenever user hits multiple keys at the same time
+  //    */
+  //   this.setState({ pressed: event.key })
 
-    // Prevent 'space' character
-    if (event.key === " ") {
-      event.preventDefault()
-    }
-  }
+  //   // Prevent 'space' character
+  //   if (event.key === " ") {
+  //     event.preventDefault()
+  //   }
+  // }
 
-  handleKeyUp(event) {
-    // Exit early if previous key is not yet handled
-    if (event.key !== this.state.pressed) {
-      return
-    }
+  // handleKeyUp(event) {
+  //   // Exit early if previous key is not yet handled
+  //   if (event.key !== this.state.pressed) {
+  //     return
+  //   }
 
-    // Focus on previous input element (if row has any previous one)
-    if (event.key === "Backspace" && event.target.value.length === 0) {
-      if (!event.target.id.endsWith("0")) {
-        const previous = this.getSibling(event.target.id, "backward")
-        this[previous].focus()
-      }
-    } else {
-      if (event.key.length === 1) {
-        const next = this.getSibling(event.target.id, "forward")
-        if (this[next]) this[next].focus()
-      }
-    }
-  }
+  //   // Focus on previous input element (if row has any previous one)
+  //   if (event.key === "Backspace" && event.target.value.length === 0) {
+  //     if (!event.target.id.endsWith("0")) {
+  //       const previous = this.getSibling(event.target.id, "backward")
+  //       this[previous].focus()
+  //     }
+  //   } else {
+  //     if (event.key.length === 1) {
+  //       const next = this.getSibling(event.target.id, "forward")
+  //       if (this[next]) this[next].focus()
+  //     }
+  //   }
+  // }
 
-  getSibling(id, direction) {
-    const lastChar = parseInt(id[id.length - 1], 10)
-    if (direction === "forward") {
-      return id.replace(/.$/, `${lastChar + 1}`)
-    } else if (direction === "backward") {
-      return id.replace(/.$/, `${lastChar - 1}`)
-    }
-  }
+  // getSibling(id, direction) {
+  //   const lastChar = parseInt(id[id.length - 1], 10)
+  //   if (direction === "forward") {
+  //     return id.replace(/.$/, `${lastChar + 1}`)
+  //   } else if (direction === "backward") {
+  //     return id.replace(/.$/, `${lastChar - 1}`)
+  //   }
+  // }
 
   render() {
-    const height = data.length
-
-    let left = 0
-    let right = 0
-
-    const grid = []
-
-    for (let i = 0; i < height; i++) {
-      grid[i] = []
-      const index = data[i].findIndex(item => item.includes("_"))
-      const after = data[i].length - index
-
-      if (left < index) left = index
-      if (right < after) right = after
-    }
-
-    const width = left + right
-
-    grid.forEach((row, rowIndex) => {
-      const index = data[rowIndex].findIndex(item => item.includes("_"))
-      const wordLength = data[rowIndex].length
-
-      let leftStart
-      let diff
-
-      for (let i = 0; i < width; i++) {
-        if (i < left - index || i > left - index + wordLength - 1) {
-          row[i] = null
-        } else {
-          /* Store difference between left and first letter's index */
-          if (i - left < 0 && !leftStart) {
-            leftStart = true
-            diff = left - i
-          }
-
-          if (leftStart) row[i] = [rowIndex, i - left + diff]
-          else row[i] = [rowIndex, i - left]
-        }
-      }
-    })
-
-    let labelIndex
-    let currentRow
-
     return (
       <form className={styles.grid} ref={el => (this._form = el)}>
-        {grid.map((row, rowIndex) => {
+        {data.map((row, rowIndex) => {
           return (
             <div className={styles.row} key={`row-${rowIndex}`}>
               {row.map((cell, cellIndex) => {
-                if (!cell)
+                // Only add bottom and right border on those cells that do
+                // not have an item to the right or below
+                const next = data[rowIndex][cellIndex + 1]
+                const prev = data[rowIndex][cellIndex - 1]
+                let below
+                let above
+                if (data[rowIndex - 1]) above = data[rowIndex - 1][cellIndex]
+                if (data[rowIndex + 1]) below = data[rowIndex + 1][cellIndex]
+
+                if (cell === "empty") {
                   return (
                     <div
                       key={`placeholder-${cellIndex}`}
-                      className={styles.placeholder}
+                      className={`
+                      ${styles.placeholder}
+                      ${(prev === "empty" || !prev) &&
+                        styles.placeholderLeftBorder}
+                      ${(above === "empty" || !above) &&
+                        styles.placeholderTopBorder}
+                        ${!next && styles.rightBorder}
+                        ${!below && styles.bottomBorder}
+                      `}
                     ></div>
                   )
-
-                const value = data[cell[0]][cell[1]]
-                const vertical = value.includes("_")
-
-                if (currentRow !== rowIndex) {
-                  labelIndex = cellIndex
-                  currentRow = rowIndex
                 }
 
+                let label = cell.includes("*")
+                if (label) label = cell.split("*").pop()
+
                 return (
-                  <div
-                    key={`cell-${cellIndex}`}
-                    className={styles.cell}
-                    ref={el => (!this._cell ? (this._cell = el) : null)}
-                  >
-                    {labelIndex === cellIndex ? (
-                      <label className={styles.label}>{rowIndex + 1}</label>
+                  <div key={`cell-${cellIndex}`} className={styles.cell}>
+                    {label ? (
+                      <label className={styles.label}>{label}</label>
                     ) : null}
                     <input
                       ref={el => (this[`input_${cell[0]}_${cell[1]}`] = el)}
                       onChange={this.handleChange}
                       id={`input_${cell[0]}_${cell[1]}`}
-                      className={`${styles.input} ${
-                        vertical ? styles.vertical : null
-                      }`}
+                      className={`
+                        ${styles.input}
+                        ${(!next || next === "empty") && styles.rightBorder}
+                        ${(!below || below === "empty") && styles.bottomBorder}
+                      `}
                       type="text"
-                      onKeyDown={this.handleKeyDown}
-                      onKeyUp={this.handleKeyUp}
                       minLength="1"
-                      required={vertical}
+                      required={cell.includes("_")}
                       maxLength="1"
                       autoComplete="off"
                       pattern="^[a-öA-Ö]{1}$"
