c5299be4f03b6db3d52f312fdcee873d0b63fb2e
Katarina Ljungdahl <katarina@earthpeople.se>
2019-07-15 19:03:19 +0200

    Add support for submitting the answers once crossword is complete

--- src/components/form.js
+++ src/components/form.js
@@ -1,10 +1,45 @@
 import React from "react"
 import styles from "./form.module.css"
 
-const SubmitForm = () => {
+import Input from "./input"
+import Button from "./button"
+
+const SubmitForm = ({ handleSubmit, loading, submitted }) => {
   return (
-    <form>
-      <h2 className={styles.title}>Du har fyllt i allt!</h2>
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <div className={styles.content}>
+        <h2 className={styles.title}>Skicka in ditt svar</h2>
+        <Input
+          submitted={submitted}
+          name="tie"
+          label="Svar på utslagsfråga"
+          type="text"
+        />
+        <Input submitted={submitted} name="name" label="Namn" type="text" />
+        <Input
+          submitted={submitted}
+          name="email"
+          label="E-postadress"
+          type="email"
+        />
+        <Input
+          submitted={submitted}
+          name="phone"
+          label="Telefonnummer"
+          type="tel"
+        />
+        <Button
+          type="submit"
+          label={submitted ? "Skickat." : "Skicka"}
+          loading={loading}
+          disabled={submitted}
+        />
+        {submitted ? (
+          <div className={styles.message}>
+            <p>Ditt svar är inskickat. Tack!</p>
+          </div>
+        ) : null}
+      </div>
     </form>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,11 @@ class Main extends Component {
 
     this.state = {
       filled: false,
+      submitted: false,
+      solution: [],
     }
+
+    this.handleChange = this.handleChange.bind(this)
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -24,14 +28,65 @@ class Main extends Component {
     }
   }
 
+  handleChange(valid, { id, value }) {
+    if (id.includes("_")) {
+      const clone = this.state.solution.slice()
+      const current = id.split("-").pop()
+      const index = clone.findIndex(el => el.index === current)
+
+      if (index === -1) {
+        clone.push({ index: current, value })
+      } else {
+        if (value === "") {
+          clone.splice(index, 1)
+        } else {
+          clone[index].value = value
+        }
+      }
+
+      if (valid) {
+        this.setState({ filled: valid, solution: clone })
+      } else {
+        this.setState({ solution: clone })
+      }
+    }
+  }
+
+  handleSubmit(event) {
+    this.setState({ loading: true })
+    event.preventDefault()
+    const str = this.state.solution
+      .sort((a, b) => (b.index - a.index) * -1)
+      .reduce((acc, current) => (acc += current.value), "")
+
+    const data = new FormData(event.target)
+
+    data.set(process.env.SOLUTION_FIELD, str.toUpperCase())
+    data.set(process.env.TIE_FIELD, data.get("tie"))
+    data.set(process.env.NAME_FIELD, data.get("name"))
+    data.set(process.env.EMAIL_FIELD, data.get("email"))
+    data.set(process.env.PHONE_FIELD, data.get("phone"))
+
+    fetch(process.env.FORM_URL, {
+      method: "POST",
+      mode: "no-cors",
+      body: data,
+    }).then(() => this.setState({ submitted: true, loading: false }))
+  }
+
   render() {
     return (
       <Layout>
         <SEO title="Schlagerord-flätan" />
-        <Crossword onChange={bool => this.setState({ filled: bool })} />
-        {this.state.filled ? (
+        <Crossword onChange={this.handleChange} />
+        {!this.state.filled ? (
           <div ref={el => (this._form = el)}>
-            <SubmitForm />
+            <SubmitForm
+              submitted={this.state.submitted}
+              loading={this.state.loading}
+              onInput={this.handleInput}
+              handleSubmit={event => this.handleSubmit(event)}
+            />
           </div>
         ) : null}
       </Layout>
