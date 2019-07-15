46adc14716a787548a4059f2fd2c35e990955f03
Katarina Ljungdahl <katarina@earthpeople.se>
2019-07-15 19:02:18 +0200

    Disable form elements once form is submitted

--- src/components/input.js
+++ src/components/input.js
@@ -2,10 +2,11 @@ import React from "react"
 
 import styles from "./input.module.css"
 
-const Input = ({ name, type, label }) => (
+const Input = ({ name, type, label, submitted }) => (
   <div className={styles.container}>
     <input
-      className={styles.input}
+      disabled={submitted}
+      className={`${styles.input} ${submitted && styles.submitted}`}
       type={type}
       id={name}
       name={name}
